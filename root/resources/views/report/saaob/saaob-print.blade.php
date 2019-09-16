02f59988560ed9e8112f74505828a74371353fc3
rightappswarriors <rightappsservices@gmail.com>
2019-09-16 16:24:47 +0800

    SAOB report modify

--- root/resources/views/report/saaob/saaob-print.blade.php
+++ root/resources/views/report/saaob/saaob-print.blade.php
@@ -26,35 +26,31 @@
       <div class="row">
         <center>
         <div class="col-sm-12">
-          <table class="table table-bordered" style="border: 1px solid #000;">
+          <table class="table table-bordered">
             <thead>
               <tr>
-                <th height="50" style="border: 1px solid #000; width:15%; "><center>Account Code</center></th>
-                <th height="50" style="border: 1px solid #000; width:90%; "><center>Function/Program/Project</center></th>
-                <th height="50" style="border: 1px solid #000; width:20%; "><center>Appropriation</center></th>
-                <th height="50" style="border: 1px solid #000; width:20%; "><center>Allotment</center></th>
-                <th height="50" style="border: 1px solid #000; width:20%; "><center>Obligation</center></th>
-                <th height="50" style="border: 1px solid #000; width:20%; " colspan="2"><center>Balances of</center></th>
+                <th rowspan="2" nowrap style="padding: 0rem 10px 3rem 10px;" class="text-center">Account Code</th>
+                <th rowspan="2" style="padding: 0rem 10px 3rem 10px;" class="text-center fpp">Function/Program/Project</th>
+                <th rowspan="2" style="font-size: 12px !important;padding: 0rem 10px 3rem 10px;" class="text-center">Appropriation</th>
+                <th rowspan="2" style="font-size: 12px !important;padding: 0rem 10px 3rem 10px;" class="text-center">Allotment</th>
+                <th rowspan="2" style="font-size: 12px !important;padding: 0rem 10px 3rem 10px;" class="text-center">Obligation</th>
+                <th colspan="2" class="text-center">Balances of</th>
               </tr>
-            </thead>
+              <tr>
+                <th style="font-size: 12px !important;"><center>APPROPRIATIONS</center></th>
+                <th style="font-size: 12px !important;"><center>ALLOTMENTS</center></th>
+              </tr>
+            </thead>          
             <tbody>
+              
               <tr class="noborder">
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td><b>APPROPRIATIONS</b></td>
-                <td><b>ALLOTMENTS</b></td>
-              </tr>
-              <tr class="noborder">
-                <td></td>
-                <td height="50"><b><u><center>CURRENT YEAR APPROPRIATION</center></u></b></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td  style="border-bottom:none !important;border-top:none !important;"></td>
+                <td  style="border-bottom:none !important;border-top:none !important;"><b><u><center>CURRENT YEAR APPROPRIATION</center></u></b></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
               </tr>
 
               @php
@@ -64,26 +60,26 @@
               @isset($Function)
               @foreach($Function as $F)
               <tr class="noborder">
-                <td><center><b>{{$F->funcid}}</b></center></td>
-                <td height="50" style="text-indent: 20px;"><b>{{strtoupper($F->funcdesc)}}</b></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td style="border-bottom:none !important;border-top:none !important;"><center><b>{{$F->funcid}}</b></center></td>
+                <td style="border-bottom:none !important;border-top:none !important;text-indent: 20px;"><b>{{strtoupper($F->funcdesc)}}</b></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
               </tr>
               @isset($Header)
               @foreach($Header as $h)
 
               @if($F->funcid == $h->funcid)
               <tr class="noborder">
-                <td><center><b>{{$h->office_code}}</b></center></td>
-                <td><b><u>{{strtoupper($h->office)}}</u></b></td>
-                <td ></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td style="border-bottom:none !important;border-top:none !important;"><center><b>{{$h->office_code}}</b></center></td>
+                <td style="border-bottom:none !important;border-top:none !important;"><b><u>{{strtoupper($h->office)}}</u></b></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
+                <td style="border-bottom:none !important;border-top:none !important;"></td>
               </tr>
                 
 
@@ -96,21 +92,21 @@
                 @if($h->b_num == $P->b_num) {{-- HEADER == PPA --}}
                   
                   <tr class="noborder">
-                    <td></td>
-                    <td height="50" style="text-indent: 10px; vertical-align: bottom;"><b>{{$P->subgrpdesc}}</b></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                     <td></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"></td>
+                    <td style="border-bottom:none !important;border-top:none !important;text-indent: 10px; vertical-align: bottom;"><b>{{$P->subgrpdesc}}</b></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"></td>
+                     <td style="border-bottom:none !important;border-top:none !important;"></td>
                   </tr>
 
                   @foreach($Line as $L) {{-- LINE --}}
                   @if($h->b_num == $L->b_num)
                   @if($P->grpid == $L->grpid)
                   <tr class="noborder noborder2">
-                    <td><center>{{$L->at_code}}</center></td>
-                    <td>{{$L->at_desc}}</td>
+                    <td style="border-bottom:none !important;border-top:none !important;"><center>{{$L->at_code}}</center></td>
+                    <td style="border-bottom:none !important;border-top:none !important;">{{$L->at_desc}}</td>
                     <td align="right">{{number_format($L->appro_amnt, 2)}}</td>
                     <td align="right">{{number_format($L->allot_amnt, 2)}}</td>
                     <td align="right">{{number_format($L->obr, 2)}}</td>
@@ -128,13 +124,13 @@
                   @endforeach {{-- LINE --}}
 
                   <tr class="noborder noborder3">
-                    <td></td>
-                    <td><b>Total {{$P->subgrpdesc}}</b></td>
-                    <td align="right"><b>{{number_format($total_ppaamt, 2)}}</b></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"></td>
+                    <td style="border-bottom:none !important;border-top:none !important;"><b>Total {{$P->subgrpdesc}}</b></td>
+                    <td style="border-bottom:2px solid !important;border-top:2px solid !important;"align="right"><b>{{number_format($total_ppaamt, 2)}}</b></td>
+                    <td style="border-bottom:2px solid !important;border-top:2px solid !important;"align="right"></td>
+                    <td style="border-bottom:2px solid !important;border-top:2px solid !important;"align="right"></td>
+                    <td style="border-bottom:2px solid !important;border-top:2px solid !important;"align="right"></td>
+                    <td style="border-bottom:2px solid !important;border-top:2px solid !important;"align="right"></td>
                   </tr>
                   @php
                   // $total_amt += $P->total_amt;
@@ -150,8 +146,8 @@
               @endisset    
                   <tr class="noborder noborder3">
                     <td></td>
-                    <td height="50" style="vertical-align: bottom;"><b>GRAND TOTAL</b></td>
-                    <td align="right" height="50" style="vertical-align: bottom;"><b>{{number_format($grandtotal, 2)}}</b></td>
+                    <td style="vertical-align: bottom;"><b>GRAND TOTAL</b></td>
+                    <td align="right" style="vertical-align: bottom;"><b>{{number_format($grandtotal, 2)}}</b></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -160,21 +156,7 @@
               
             </tbody>
           </table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          
+ 
         </div>
       </center>
       </div>
@@ -186,12 +168,13 @@
         
         .table th{
         background-color: transparent !important;
-        border: 1px solid #000 !important;
+        border: 1px solid !important;
+        font-size: 13px !important;
       }
 
       .table td{
         background-color: transparent !important;
-        border: 1px solid #000 !important;
+        border: 1px solid !important;
         /*border-right:none !important;
 border-left:none !important;*/
 
@@ -200,19 +183,19 @@ border-left:none !important;*/
       /*table > tbody > td .hideborder{
         border: hidden;
       }*/
-      tr.noborder td{
+/*      tr.noborder td{
         border-bottom:none !important;
         border-top:none !important;
       }
 
       tr.noborder2 td:last-child {
-        border-bottom: 1px solid #000 !important;
-        border-top: 1px solid #000 !important;
-      }
+        border-bottom: 1px solid  !important;
+        border-top: 1px solid  !important;
+      }*/
 
       tr.noborder3 td:last-child {
-        border-bottom: 2px solid #000 !important;
-        border-top: 2px solid #000 !important;
+        border-bottom: 1px solid !important;
+        border-top: 1px solid !important;
       }
 
       /*tr.noborder td{
