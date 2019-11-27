392dbdd6550c8f1251543f99b5a54fd3cdb77217
rightappswarriors <rightappsservices@gmail.com>
2019-11-27 15:39:13 +0800

    Guihulngan : Stock Transaction Card 11/27/2019 --Deniel

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/root/vendor/autoload.php';
+require __DIR__.'./root/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/root/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/root/bootstrap/app.php';
+$app = require_once __DIR__.'./root/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
